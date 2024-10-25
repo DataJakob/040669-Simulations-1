e72d1ac70be89e7aaec1178b25538d9247997c86
DataJakob <lindstromjakob@hotmail.com>
2024-10-25 11:43:23 +0200

    Reframing repo sturcture and creating  a skeleton for next weeks homework

--- Homeworks/02/task_three.jl
+++ Homeworks/02/task_three.jl
@@ -10,6 +10,10 @@ It has a limited functionality according to examples that
 have been presented in the course so far. 
 """
 
+
+"""
+State input parameters
+"""
 arbitrary_person_que_length = 5
 n_customers = 7
 interarrival_times = [0.6, 0.3, 0.5, 0.2, 0.7, 0.3, 0.4]
@@ -39,81 +43,6 @@ function calculate_ia_and_s_times(
 end
 
 
-function caluclate_avg_waiting_time(
-    arbitrary_person_que_length::Int,
-    calculate_ia_and_s_times::Function,
-    ia_times::Vector{Float64}, 
-    s_times::Vector{Float64},
-    )
-    """
-    Calcualte average  waiting time that an abritrary person 
-    must stand in line for
-    """
-    cum_ia_times, cum_s_times = calculate_ia_and_s_times(
-        ia_times, s_times
-        )
-    
-    total_waiting_time = 0
-    inside_que = Float64[]
-    busy = false
-    calculation_leaver_time = cum_s_times[arbitrary_person_que_length]
-    process =  true
-
-    while process == true
-        if length(cum_ia_times) == 0
-            arrival = Inf
-        else
-            arrival = cum_ia_times[1]
-        end
-        departure = cum_s_times[1]
-        
-        # Continue the line
-        if departure > calculation_leaver_time || length(cum_s_times) == 0
-            process = false
-        end
-
-        # Arrival happens next
-        if arrival < departure
-            # Not busy -> no recording
-            if busy == false
-                popfirst!(cum_ia_times)
-                busy = true
-            # Busy -> start recording
-            else
-                push!(inside_que, deepcopy(arrival))
-                popfirst!(cum_ia_times)
-                busy = true
-            end
-        
-        # Departure happens next
-        else
-            # If no in queue -> no recording, set busy to false
-            if length(inside_que) == 0
-                popfirst!(cum_s_times)
-                busy = false
-            
-            # Someone in que -> calculate w-time and potential rest time
-            else
-                individual_que_time = departure - inside_que[1]
-                total_waiting_time += individual_que_time
-                popfirst!(inside_que)
-                popfirst!(cum_s_times)
-                busy = true        
-
-                if (length(cum_s_times) >= 1) && (cum_s_times[1] > calculation_leaver_time)
-                    rest_sum = calculation_leaver_time .-inside_que
-                    total_waiting_time += sum(rest_sum)
-                    process = false
-                end
-            end
-        end
-    end
-    return  total_waiting_time/calculation_leaver_time
-end
-
-
-
-
 function caluclate_avg_que_length(
     arbitrary_person_que_length::Int64,
     calculate_ia_and_s_times::Function,
@@ -136,6 +65,85 @@ function caluclate_avg_que_length(
     return que_length / arbitrary_person_que_length
 end
 
+
+function caluclate_avg_waiting_time(
+    arbitrary_person_que_length::Int,
+    calculate_ia_and_s_times::Function,
+    ia_times::Vector{Float64}, 
+    s_times::Vector{Float64},
+    )
+    """
+    Calcualte average  waiting time that an abritrary person 
+    must stand in line for
+    """
+    cum_ia_times, cum_s_times = calculate_ia_and_s_times(
+        ia_times, s_times
+        )
+    
+    # Stating start parameters.
+    total_waiting_time = 0
+    inside_que = Float64[]
+    busy = false
+    calculation_leaver_time = cum_s_times[arbitrary_person_que_length]
+    process =  true
+
+    # Define arrival and departure times.
+    while process == true
+        if length(cum_ia_times) == 0
+            arrival = Inf
+        else
+            arrival = cum_ia_times[1]
+        end
+        departure = cum_s_times[1]
+        
+        # Decides whether to continue the loop
+        if departure > calculation_leaver_time || length(cum_s_times) == 0
+            process = false
+        end
+
+        # If arrival is the next event happening
+        if arrival < departure
+            # Not busy -> no recording
+            if busy == false
+                popfirst!(cum_ia_times)
+                busy = true
+            # Busy -> start recording
+            else
+                push!(inside_que, deepcopy(arrival))
+                popfirst!(cum_ia_times)
+                busy = true
+            end
+        
+        # If departure happens next
+        else
+            # If no in queue -> no recording, set busy to false
+            if length(inside_que) == 0
+                popfirst!(cum_s_times)
+                busy = false
+            
+            # Someone in que -> calculate waiting time and potential rest time.
+            else
+                individual_que_time = departure - inside_que[1]
+                total_waiting_time += individual_que_time
+                popfirst!(inside_que)
+                popfirst!(cum_s_times)
+                busy = true        
+
+                if (length(cum_s_times) >= 1) && (cum_s_times[1] > calculation_leaver_time)
+                    rest_sum = calculation_leaver_time .-inside_que
+                    total_waiting_time += sum(rest_sum)
+                    process = false
+                end
+            end
+        end
+    end
+    return  total_waiting_time/calculation_leaver_time
+end
+
+
+
+
+# Print output
 a = calculate_ia_and_s_times(interarrival_times, service_times)
 
 b = caluclate_avg_que_length(
---
+++ Homeworks/03/compFiles/rng.jl
@@ -0,0 +1,6 @@
+
+# Create at two sets of distributions.
+#   One for interarrival times
+#   One for service times
+#
+# Input shall be: [dist_type, my, sigma, n_customers, n_arrays]
